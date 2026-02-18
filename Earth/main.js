990e017c57a3d7de64d4239f45f7311eb3a3e670
Jose Ferreira <jpedroferreira95@gmail.com>
2026-02-18 14:17:44 +0000

    add earth texture

--- Earth/main.js
+++ Earth/main.js
@@ -12,10 +12,11 @@ const near = 0.1;
 const far = 100;
 
 const camera = new THREE.PerspectiveCamera(fieldOfView, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer();
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 renderer.setSize(width, height);
 
@@ -24,14 +25,18 @@ document.body.appendChild(renderer.domElement);
 const textureLoader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 6);
 const material = new THREE.MeshStandardMaterial({
-  map: textureLoader.load("/images/apple.jpg"),
+  map: textureLoader.load("images/earthmap1k.jpg"),
 });
 
 const earth = new THREE.Mesh(geometry, material);
 scene.add(earth);
 
-const hemisphereLight = new THREE.HemisphereLight(0x000000, 0xffffff);
-scene.add(hemisphereLight);
+// const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff);
+// scene.add(hemisphereLight);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 3);
+sunLight.position.set(-2, -0.5, 1.5);
+scene.add(sunLight);
 
 function animate() {
   earth.rotation.y += 0.001;
